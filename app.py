0bab850913d65f3492ceded14106f42bb41a05f9
Navpreet Singh <87243405+Navpreet11@users.noreply.github.com>
2024-07-19 14:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ def submit():
         return render_template("thankyou.html")
         
     return render_template("submit.html")
-app.run(debug=True,host="0.0.0.0",port=8000)
+#app.run(debug=True,host="0.0.0.0",port=8000)
     
 #{{url_for('addflag',answerid=i['answers'])}}
 
